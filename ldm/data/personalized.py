5f59e8f66cf59fc165a12f8c8f8730b32c191486
Chunk Norrmis <128258022+ChunkNorrmis@users.noreply.github.com>
2025-05-09 09:47:31 -0400

    Update personalized.py

--- ldm/data/personalized.py
+++ ldm/data/personalized.py
@@ -91,7 +91,7 @@ class PersonalizedBase(Dataset):
         
         image = self.flip(image)
         
-image = np.array(image).astype(np.uint8)
+        image = np.array(image).astype(np.uint8)
         example["image"] = (image / 127.5 - 1.0).astype(np.float32)
         
         return example
